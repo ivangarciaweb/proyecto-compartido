3f9091277e652911ed0e02fc8e37462a5e5489b6
Iván García <i_garcia@outlook.es>
2017-10-25 21:15:27 +0200

    js practice 1

---
+++ tablas_multiplicar.js
@@ -0,0 +1,72 @@
+for (contador = 1; contador <= 10; contador++){	
+	if (contador == 1){		
+		console.log('Tabla del 1:');
+		for(operando = 1; operando <= 10; operando++){
+			console.log('1 X ' + operando + ' = ' + operando*1);
+		}
+		console.log(' ');
+	}
+	if (contador == 2){
+		console.log('Tabla del 2:');
+		for(operando = 1; operando <= 10; operando++){
+			console.log('2 X ' + operando + ' = ' + operando*2);
+		}
+		console.log(' ');
+	}
+	if (contador == 3){
+		console.log('Tabla del 3:');
+		for(operando = 1; operando <= 10; operando++){
+			console.log('3 X ' + operando + ' = ' + operando*3);
+		}
+		console.log(' ');
+	}
+	if (contador == 4){
+		console.log('Tabla del 4:');
+		for(operando = 1; operando <= 10; operando++){
+			console.log('4 X ' + operando + ' = ' + operando*4);
+		}
+		console.log(' ');
+	}
+	if (contador == 5){
+		console.log('Tabla del 5:');
+		for(operando = 1; operando <= 10; operando++){
+			console.log('5 X ' + operando + ' = ' + operando*5);
+		}
+		console.log(' ');
+	}
+	if (contador == 6){
+		console.log('Tabla del 6:');
+		for(operando = 1; operando <= 10; operando++){
+			console.log('6 X ' + operando + ' = ' + operando*6);
+		}
+		console.log(' ');
+	}
+	if (contador == 7){
+		console.log('Tabla del 7:');
+		for(operando = 1; operando <= 10; operando++){
+			console.log('7 X ' + operando + ' = ' + operando*7);
+		}
+		console.log(' ');
+	}
+	if (contador == 8){
+		console.log('Tabla del 8:');
+		for(operando = 1; operando <= 10; operando++){
+			console.log('8 X ' + operando + ' = ' + operando*8);
+		}
+		console.log(' ');
+	}
+	if (contador == 9){
+		console.log('Tabla del 9:');
+		for(operando = 1; operando <= 10; operando++){
+			console.log('9 X ' + operando + ' = ' + operando*9);
+		}
+		console.log(' ');
+	}
+	if (contador == 10){
+		console.log('Tabla del 10:');
+		for(operando = 1; operando <= 10; operando++){
+			console.log('10 X ' + operando + ' = ' + operando*10);
+		}
+		console.log(' ');
+	}
+}
