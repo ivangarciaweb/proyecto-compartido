bde3c81d7d473292374e02f7d362d8cdeb862b91
Javascript <javascript@zPro3.local>
2017-10-26 21:39:11 +0200

    añadido al archivo otras opciones de hacerlo

--- Practicas/prueba-impar-siguientes-50.js
+++ Practicas/prueba-impar-siguientes-50.js
@@ -8,3 +8,47 @@ function numerosImpares(num){
 }
 
 numerosImpares(20)
+
+//Mejorada
+
+function showOdds (number){//declaro la funcion
+	if(number % 2 != 0){//miro si el numero es impar
+		var limit = number + 50;//declaro un limite
+		for(number;number<limit;number=number+2){//preparo el bucle
+			console.log(number);//imprimo el numero
+		}
+	}	
+	else{
+		number++; //En caso de que sea par le añado 1
+		showOdds(number);	//le vuelvo a pasar la misma función
+		
+	}
+}
+showOdds(20)
+
+//Mejorada con 2 funciones
+function isPair(number){
+	if(number % 2 != 0){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+
+function showOdds(number){
+	var limit = number + 50;
+	if(isPair(number)){
+		number++;
+		for(number; number < limit; number= number + 2){
+			console.log(number);
+		}
+	}
+	else{
+		for(number; number <= limit; number= number + 2){
+			console.log('hola');
+		}
+	}
+}
+
+showOdds(20)
